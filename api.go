2460b7c41c2384eee611a9f9aff36ffe914faa24
BrandonC98 <brandoncampbell98@hotmail.co.uk>
2024-01-06 00:59:32 +0000

    Adds api endpoints

---
+++ api.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/ProtonMail/gopenpgp/v2/helper"
+	"github.com/gin-gonic/gin"
+)
+
+func router() *gin.Engine {
+	return gin.Default()
+}
+
+func pingHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong",
+	})
+}
+
+func generateHandler(config Config) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var key string
+		if gin.Mode() == "release" {
+			// use AWS Secrets manager to get key
+		} else {
+			key = "PASSMAN_PASS_GEN_KEY"
+		}
+
+		plaintextPassword, err := generatePassword(randNumber(config.StringMinLength, config.StringMaxLength))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		ecryptedPassword, err := helper.EncryptMessageWithPassword([]byte(key), plaintextPassword)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": ecryptedPassword,
+		})
+	}
+}
+
+func startServer() {
+	router := router()
+
+	router.GET("/ping", pingHandler)
+
+	router.GET("/generate", generateHandler(config))
+
+	err := router.Run(fmt.Sprintf(":%d", config.Port))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ api_test.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPingEndpoint(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	var tests = []struct {
+		name        string
+		endpoint    string
+		requestType string
+		statusCode  int
+		body        string
+	}{
+		{"successfully hit the ping endpoint", "/ping", "GET", 200, `{"message":"pong"}`},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			router := gin.Default()
+			router.GET(test.endpoint, pingHandler)
+
+			req, err := http.NewRequest(test.requestType, test.endpoint, nil)
+			assert.Nil(t, err)
+
+			recorder := httptest.NewRecorder()
+			router.ServeHTTP(recorder, req)
+
+			assert.Equal(t, test.statusCode, recorder.Code)
+			assert.Equal(t, test.body, recorder.Body.String())
+		})
+	}
+}
+
+func TestGenerateEndpoint(t *testing.T) {
+	config = Config{
+		Mode:            "test",
+		Port:            8080,
+		StringMinLength: 7,
+		StringMaxLength: 25,
+	}
+	gin.SetMode(config.Mode)
+	var tests = []struct {
+		name         string
+		endpoint     string
+		requestType  string
+		statusCode   int
+		bodyContains []string
+	}{
+		{"successfully hit the generate endpoint", "/generate", "GET", 200, []string{"BEGIN PGP MESSAGE", "END PGP MESSAGE"}},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+
+			router := gin.Default()
+			router.GET(test.endpoint, generateHandler(config))
+
+			req, err := http.NewRequest(test.requestType, test.endpoint, nil)
+			assert.Nil(t, err)
+			recorder := httptest.NewRecorder()
+			router.ServeHTTP(recorder, req)
+
+			assert.Equal(t, test.statusCode, recorder.Code)
+			for _, expectedString := range test.bodyContains {
+				assert.Contains(t, recorder.Body.String(), expectedString)
+			}
+		})
+	}
+}
