ad9c5085f1f4d678a295e08ccaabfc41b0e323e0
BrandonC98 <brandoncampbell98@hotmail.co.uk>
2024-02-11 15:34:57 +0000

    Fixes js issue'

--- static/js/main.js
+++ static/js/main.js
@@ -2,8 +2,7 @@
 function generatePassword() {
 		fetch('/generatePassword')
 				.then(response => response.json())
-				.then(data => {
-						document.getElementById('passwordField').value = data.password;
-
+				.then((data) => {
+						document.getElementById('passwordField').value = data.message;
 				}).catch(error => console.error('Error:', error));
 }
